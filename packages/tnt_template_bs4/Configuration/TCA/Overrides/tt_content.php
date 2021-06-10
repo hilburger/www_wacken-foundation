fbdb381cff5e6e077ed5cc0fefb58e2963d7d64e
tnt_hilburger <hilburger@tnt-digitalagentur.de>
2021-06-11 00:14:29 +0200

    [TASK] Add a third 2-col-layout

--- packages/tnt_template_bs4/Configuration/TCA/Overrides/tt_content.php
+++ packages/tnt_template_bs4/Configuration/TCA/Overrides/tt_content.php
@@ -75,6 +75,24 @@ $GLOBALS['TCA']['tt_content']['types']['1col-container']['previewRenderer'] = 'B
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)-> configureContainer(
     (
         new \B13\Container\Tca\ContainerConfiguration(
+            '2col-container-33-66',
+            '2 Spalten: 33% zu 66%',
+            '2 Spalten in 33% zu 66%',
+            [
+                [
+                    ['name' => 'Linke Spalte', 'colPos' => 201],
+                    ['name' => 'Rechte Spalte', 'colPos' => 202, 'colspan' => 2]
+                ]
+            ]
+        )
+    )
+    ->setIcon('EXT:container/Resources/Public/Icons/container-2col-right.svg')
+    //->setBackendTemplate('EXT:tnt_template_bs4/Resources/Private/Extensions/container/Templates/2col-container_be.html')
+);
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)-> configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
             '3col-container',
             '3 Spalten',
             '3 Spalten je 33%',
