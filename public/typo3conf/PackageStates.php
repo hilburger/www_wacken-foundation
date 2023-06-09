c5387f3ef4e23d3507506dfbc1ed79e0406bf2f1
Thomas Hilburger <hilburger@tnt-digitalagentur.de>
2023-06-09 10:20:13 +0200

    [TASK] Remove Ext numbered_pagination again

--- public/typo3conf/PackageStates.php
+++ public/typo3conf/PackageStates.php
@@ -107,12 +107,6 @@ return [
         'viewpage' => [
             'packagePath' => 'typo3/sysext/viewpage/',
         ],
-        'numbered_pagination' => [
-            'packagePath' => 'typo3conf/ext/numbered_pagination/',
-        ],
-        'news' => [
-            'packagePath' => 'typo3conf/ext/news/',
-        ],
         'bootstrap_package' => [
             'packagePath' => 'typo3conf/ext/bootstrap_package/',
         ],
@@ -124,6 +118,9 @@ return [
         ],
         'ke_search' => [
             'packagePath' => 'typo3conf/ext/ke_search/',
+        ],
+        'news' => [
+            'packagePath' => 'typo3conf/ext/news/',
         ],
         'sf_banners' => [
             'packagePath' => 'typo3conf/ext/sf_banners/',
